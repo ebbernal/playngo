7553a0f1519b39092c82630dce268ab744dc5bbc
ebbernal <edison.b.bernal@gmail.com>
2018-06-07 17:08:40 +0800

    Initial commit: MVC with EF and Hub

---
+++ PlayNGo/PlayNGo/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace PlayNGo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+    }
+}   
---
+++ PlayNGo/PlayNGo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlayNGo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
